9ddbfe5c777512c1866b9357690b284c59710fdf
dhirajchande <dhirajchande@gmail.com>
2022-10-06 13:14:11 +0530

    home page improvements

--- src/app/my-courses/my-courses.component.ts
+++ src/app/my-courses/my-courses.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-my-courses',
   templateUrl: './my-courses.component.html',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyCoursesComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    
+  }
 
   ngOnInit(): void {
   }
+
+  
 
 }
